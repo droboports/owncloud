ce46b5bb063e159b4566917ba6c2a74a4e4f7d83
Ricardo Padilha <ricardospadilha@gmail.com>
2015-10-29 17:22:44 -0700

    Added a reset script

---
+++ src/dest/reset.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+#
+# reset script
+
+prog_dir="$(dirname "$(realpath "${0}")")"
+name="$(basename "${prog_dir}")"
+data_dir="/mnt/DroboFS/Shares/DroboApps/.AppData/${name}"
+tmp_dir="/tmp/DroboApps/${name}"
+logfile="${tmp_dir}/reset.log"
+
+# boilerplate
+if [ ! -d "${tmp_dir}" ]; then mkdir -p "${tmp_dir}"; fi
+exec 3>&1 4>&2 1>> "${logfile}" 2>&1
+echo "$(date +"%Y-%m-%d %H-%M-%S"):" "${0}" "${@}"
+set -o errexit  # exit on uncaught error code
+set -o nounset  # exit on unset variable
+set -o xtrace   # enable script tracing
+
+/bin/sh "${prog_dir}/service.sh" stop
+
+rm -fR "${data_dir}"
+mkdir -p "${data_dir}"
+
+/bin/sh "${prog_dir}/service.sh" start
